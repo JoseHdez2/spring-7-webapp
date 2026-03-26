bed8cea2bf93a5b4d299d35309ef7de4802faf41
Jose Hernandez <josemanuelhdez2@gmail.com>
2026-03-26 14:24:14 +0000

    feat(webapp): BookService 1

---
+++ src/main/java/guru/springframework/spring7webapp/services/BookServiceImpl.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring7webapp.services;
+
+import guru.springframework.spring7webapp.domain.Book;
+
+public class BookServiceImpl implements BookService {
+    @Override
+    public Iterable<Book> findAll() {
+        return null;
+    }
+}
