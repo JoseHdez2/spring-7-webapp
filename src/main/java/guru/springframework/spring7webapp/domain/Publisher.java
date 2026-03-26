2a94c432f10f7cacac3e9b0c5ac02023305a7681
Jose Hernandez <josemanuelhdez2@gmail.com>
2026-03-26 12:32:07 +0000

    feat(webapp): Publisher in BootstrapData

--- src/main/java/guru/springframework/spring7webapp/domain/Publisher.java
+++ src/main/java/guru/springframework/spring7webapp/domain/Publisher.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring7webapp.domain;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import java.util.Objects;
 
+@Entity
 public class Publisher {
 
     @Id
